d4665bb80fbc6500be68fc0d4b7a1c72b9ba13d1
Gianluca Galvagni <114250896+giangalv@users.noreply.github.com>
2023-01-28 13:36:55 +0100

    Update client.py

--- ros_simulation/scripts/client.py
+++ ros_simulation/scripts/client.py
@@ -1,49 +1,49 @@
 #! /usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Point, Pose, Twist
-from nav_msgs.msg import Odometry
-from ros_simulation.msg import PlanningGoal, Pos
 import actionlib
+import ros_simulation.msg
 
 class Client:
+
     def __init__(self):
-        self.pub = rospy.Publisher('/pos', Pos, queue_size=1)
-        self.client = actionlib.SimpleActionClient('/reaching_goal', ros_simulation.msg.PlanningAction)
-        self.client.wait_for_server()
-        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
-        self.goal = PlanningGoal()
-        
+        self.position = None
+        self.linear_velocity = None
+        self.pub = rospy.Publisher('/pos', ros_simulation.msg.Pos, queue_size=1)
+        rospy.Subscriber('/odom', Odometry, self.odom_callback)
+        self.action_client = actionlib.SimpleActionClient('/reaching_goal', ros_simulation.msg.PlanningAction)
+        self.action_client.wait_for_server()
+        self.goal = ros_simulation.msg.PlanningGoal()
+
     def odom_callback(self, data):
-        position = data.pose.pose.position
-        linear_velocity = data.twist.twist.linear
-        pos_msg = Pos()
-        pos_msg.x = position.x
-        pos_msg.y = position.y
-        pos_msg.vx = linear_velocity.x
-        pos_msg.vy = linear_velocity.y
-        self.pub.publish(pos_msg)
-        
-    def input_callback(self):
+        self.position = data.pose.pose.position
+        self.linear_velocity = data.twist.twist.linear
+        msg = ros_simulation.msg.Pos()
+        msg.x = self.position.x
+        msg.y = self.position.y
+        msg.vx = self.linear_velocity.x
+        msg.vy = self.linear_velocity.y
+        self.pub.publish(msg)
+
+    def goal_input(self):
+        print("Insert the goal coordinates (x,y) or type 'c' to cancel the goal, then press ENTER:")
         while not rospy.is_shutdown():
-            input_str = input("Insert the goal coordinates (x,y) or type 'c' to cancel the goal, then press ENTER:")
-            if input_str == 'c':
-                self.client.cancel_goal()
-                print("Goal cancelled")
-            else:
-                try:
-                    x, y = map(float, input_str.split(','))
-                    self.goal.target_pose.pose.position.x = x
-                    self.goal.target_pose.pose.position.y = y
-                    self.client.send_goal(self.goal)
-                except ValueError:
-                    print("Invalid input, please try again")
+            input, o, e = select.select([sys.stdin], [], [], 1)
+            if (input):
+                input = sys.stdin.readline().rstrip()
+                if input == 'c':
+                    self.action_client.cancel_goal()
+                    print("Goal cancelled")
+                else:
+                    self.goal.target_pose.pose.position.x = float(input.split(',')[0])
+                    self.goal.target_pose.pose.position.y = float(input.split(',')[1])
+                    self.action_client.send_goal(self.goal)
 
 def main():
 	# Initialize the node
 	rospy.init_node('client')
 	client = Client()
-	client.input_callback()
+	client.goal_input()
 	rospy.spin()
 
 if __name__ == '__main__':
