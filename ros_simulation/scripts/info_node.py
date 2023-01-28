8780b77a25ea0cdbb6418842c1aacf81eccecfd8
Gianluca Galvagni <114250896+giangalv@users.noreply.github.com>
2023-01-28 12:59:13 +0100

    Update info_node.py

--- ros_simulation/scripts/info_node.py
+++ ros_simulation/scripts/info_node.py
@@ -8,36 +8,34 @@ from ros_simulation.msg import Pos
 
 class InfoNode:
     def __init__(self):
-        # Set up the subscriber
-        self.subscriber = rospy.Subscriber("/pos", Pos, self.callback)
-        
-        # Desired position
-        self.des_x = rospy.get_param("~des_x",0)
-        self.des_y = rospy.get_param("~des_y",0)
-        
-        # Frequency of the node in Hz
-        self.frequency = rospy.get_param("~publish_frequency",1)
-        
-        # Time of the last print
-        self.old_time = rospy.get_rostime().to_sec() * 1000
-    
-    def callback(self, msg):
+        self.frequency = 1.0 # Frequency of the node in Hz
+        self.old_time = 0    # Time of the last print
+
+        # Get publish frequency
+        self.frequency = rospy.get_param('publish_frequency')
+        # Subscribe to the msg
+        self.odom = rospy.Subscriber('/pos', Pos, self.callback)
+
+    def callback(self, data):
         # Get the current time in milliseconds
-        current_time = rospy.get_rostime().to_sec() * 1000
-        
-        # If the time difference is greater than the period -> print the info
-        if (current_time - self.old_time) > (1000 / self.frequency):
-        
+        current_time = time.time() * 1000
+
+        # If the time difference is greater than the period print the info
+        if current_time - self.old_time > 1000 / self.frequency:
+            # Get desired position
+            des_x = rospy.get_param('des_x')
+            des_y = rospy.get_param('des_y')
+
             # Calculate the (Euclidean) distance from the desired position
-            distance = math.sqrt((self.des_x - msg.x)**2 + (self.des_y - msg.y)**2)
-            
+            distance = math.sqrt((des_x - data.x)**2 + (des_y - data.y)**2)
+
             # Calculate the average velocity
-            velocity = math.sqrt(msg.vx**2 + msg.vy**2)
-            
+            velocity = math.sqrt(data.vx**2 + data.vy**2)
+
             # Print the info
-            rospy.loginfo("Distance from desired position: %f", distance)
-            rospy.loginfo("Average velocity: %f", velocity)
-            
+            rospy.loginfo('Distance from desired position: %f', distance)
+            rospy.loginfo('Average velocity: %f', velocity)
+
             # Update old time
             self.old_time = current_time
 
