ece212e14e4158d64278e60475efa8b171cc8718
Gianluca Galvagni <114250896+giangalv@users.noreply.github.com>
2023-01-28 12:44:04 +0100

    Update client.py

--- ros_simulation/scripts/client.py
+++ ros_simulation/scripts/client.py
@@ -5,69 +5,71 @@ import math
 import time
 import sys
 import select
+import actionlib
+import actionlib.msg
 
-from geometry_msgs.msg import Point, Pose, Twist
-from ros_simulation.msg import Pos, PlanningAction, PlanningGoal
+from std_srvs.srv import *
 from nav_msgs.msg import Odometry
-from std_srvs.srv import Empty
-from actionlib import SimpleActionClient
+from geometry_msgs.msg import Point, Pose, Twist
+import ros_simulation.msg
+from ros_simulation.msg import Pos
 
-class Client:
+class Client():
 	def __init__(self):
 		# Publisher for the position of the robot
-		self.publisher = rospy.Publisher("/pos", Pos, queue_size = 1)
-		
+		self.pub = rospy.Publisher('/pos', Pos, queue_size=1)
 		# Subscriber to the odometry of the robot to get the position and velocity
-		self.odometry_subscriber = rospy.Subscriber("/odom", Odometry, self.odometry_callback)
-
+		self.odom = rospy.Subscriber('/odom', Odometry, self.callback)
 		# Create an action client
-		self.client = SimpleActionClient("/reaching_goal", PlanningAction)
-
+		self.client = actionlib.SimpleActionClient('/reaching_goal', ros_simulation.msg.PlanningAction)
 		# Wait for the action server to start
 		self.client.wait_for_server()
-	
-	def odometry_callback(self, data):
-		# Get position and linear velocity from odometry
+
+	def callback(self, data):
+		"""
+		Get position and linear velocity from odometry
+		Create a message of type Pos
+		Fill the message with the position and linear velocity
+		Publish the message
+		"""
 		position = data.pose.pose.position
 		linear_velocity = data.twist.twist.linear
-
-		# Create a message of type Pos
 		msg = Pos()
-
-		# Fill the message with the position and linear velocity
 		msg.x = position.x
 		msg.y = position.y
 		msg.vx = linear_velocity.x
 		msg.vy = linear_velocity.y
+		self.pub.publish(msg)
 
-		# Publish the message
-		self.publisher.publish(msg)
-
-	def run(self):
-		# Print the instructions
-		print("Insert the goal coordinates (x,y) or type on the keyboard 'c' to cancel the goal, then press ENTER:")
+	def action_client(self):
+		"""
+		Print instructions
+		Wait for input of goal coordinates or 'c' to cancel goal
+		Create a message of type PlanningGoal
+		Fill the message with the goal coordinates
+		Send the goal to the action server
+		"""
+		print("Insert the goal coordinates (x,y) or type 'c' to cancel the goal, then press ENTER:")
 
 		while not rospy.is_shutdown():
-			value = input()
-			# If the input is 'c' cancel the goal
-			if value == 'c':
+			input, o, e = select.select([sys.stdin], [], [], 1)
+			if (input):
+				input = sys.stdin.readline().rstrip()
+			if input == 'c':
 				self.client.cancel_goal()
 				print("Goal cancelled")
 			else:
-				# Create a message of type PlanningGoal
-				goal = PlanningGoal()
-
-				# Fill the message with the goal coordinates
-				goal.target_pose.pose.position.x = float(value.split(',')[0])
-				goal.target_pose.pose.position.y = float(value.split(',')[1])
-
-				# Send the goal to the action server
+				goal = ros_simulation.msg.PlanningGoal()
+				goal.target_pose.pose.position.x = float(input.split(',')[0])
+				goal.target_pose.pose.position.y = float(input.split(',')[1])
 				self.client.send_goal(goal)
-				
+
 def main():
-    rospy.init_node('client')
-    client = Client()
-    client.run()
+	# Initialize the node
+	rospy.init_node('client')
+	Client()
+	rospy.spin()
 
 if __name__ == '__main__':
-    main()
+	main()
+
