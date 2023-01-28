a5527374f9b2266b511667f0eb2f9ba1adc36dc7
Gianluca Galvagni <114250896+giangalv@users.noreply.github.com>
2023-01-28 11:28:43 +0100

    Update client.py

--- ros_simulation/scripts/client.py
+++ ros_simulation/scripts/client.py
@@ -5,13 +5,12 @@ import math
 import time
 import sys
 import select
-import actionlib
-import actionlib.msg
+
 from geometry_msgs.msg import Point, Pose, Twist
 from ros_simulation.msg import Pos, PlanningAction, PlanningGoal
 from nav_msgs.msg import Odometry
-from std_srvs.srv import *
-
+from std_srvs.srv import Empty
+from actionlib import SimpleActionClient
 
 class Client:
 	def __init__(self):
@@ -20,34 +19,34 @@ class Client:
 		
 		# Subscriber to the odometry of the robot to get the position and velocity
 		self.odometry_subscriber = rospy.Subscriber("/odom", Odometry, self.odometry_callback)
-		
+
 		# Create an action client
-		self.client = actionlib.SimpleActionClient("/reaching_goal", PlanningAction)
-		
+		self.client = SimpleActionClient("/reaching_goal", PlanningAction)
+
 		# Wait for the action server to start
 		self.client.wait_for_server()
-		
+	
 	def odometry_callback(self, data):
 		# Get position and linear velocity from odometry
 		position = data.pose.pose.position
 		linear_velocity = data.twist.twist.linear
-		
+
 		# Create a message of type Pos
 		msg = Pos()
-		
+
 		# Fill the message with the position and linear velocity
 		msg.x = position.x
 		msg.y = position.y
 		msg.vx = linear_velocity.x
 		msg.vy = linear_velocity.y
-		
+
 		# Publish the message
 		self.publisher.publish(msg)
-		
+
 	def run(self):
 		# Print the instructions
 		print("Insert the goal coordinates (x,y) or type on the keyboard 'c' to cancel the goal, then press ENTER:")
-		
+
 		while not rospy.is_shutdown():
 			value = input()
 			# If the input is 'c' cancel the goal
@@ -57,18 +56,18 @@ class Client:
 			else:
 				# Create a message of type PlanningGoal
 				goal = PlanningGoal()
-				
+
 				# Fill the message with the goal coordinates
 				goal.target_pose.pose.position.x = float(value.split(',')[0])
 				goal.target_pose.pose.position.y = float(value.split(',')[1])
-				
+
 				# Send the goal to the action server
 				self.client.send_goal(goal)
 				
 def main():
-	rospy.init_node('client')
-	client = Client()
-	client.run()
-	
+    rospy.init_node('client')
+    client = Client()
+    client.run()
+
 if __name__ == '__main__':
-	main()
+    main()
