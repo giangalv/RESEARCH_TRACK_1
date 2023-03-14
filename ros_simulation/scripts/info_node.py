f74a0f5ee7134d5bac8d2c867d60cf99c2e41a46
Gianluca Galvagni <gianluca.galvagni99@gmail.com>
2023-03-14 18:27:24 +0100

    DOXYGEN
    add doxygens' comments

--- ros_simulation/scripts/info_node.py
+++ ros_simulation/scripts/info_node.py
@@ -1,5 +1,27 @@
 #! /usr/bin/env python
 
+##
+# \file info_node.py
+# \brief This node subscribes to odometry information, publishes position and velocity information,
+#	sends goals to the action server, and listens for user input to set the goal coordinates. It calculates the distance
+#	from the desired position and the average velocity, and logs this information at a specified frequency.
+# \author Galvagni Gianluca
+# \version 0.1
+# \date 14/03/2023
+#
+# \details
+#
+# Subscribes to: <BR>
+#	- 'msg/Pos': Odometry information.
+#	- '/goal_topic' (geometry_msgs/Point) : Desired position to reach.
+#
+# Publishes to: <BR>
+#	[None]
+#
+# Services: <BR>
+#	[None]
+##
+
 import rospy
 import math
 import time
@@ -7,8 +29,18 @@ import time
 from ros_simulation.msg import Pos
 from geometry_msgs.msg import Point
 
+##
+# \class InfoNode
+# \brief ROS node to the robots' information (x,y,vx and vy) and prints the distance between robot and target.
+#
+# This class subscribes to odometry information, sends goals to the action server and listens for user input to set the goal coordinates.
 class InfoNode:
-	
+
+	##
+	# \brief Constructor for InfoNode class
+	#
+	# This function initializes the InfoNode class and its variables. It gets the publish frequency and initializes the target 
+	# position variables to their default values. It also subscribes to the /goal_topic and /pos topics.
 	def __init__(self):
 		# Get publish frequency
 		self.frequency = rospy.get_param('publish_frequency')
@@ -24,13 +56,23 @@ class InfoNode:
 		
 		# Subscribe to the msg
 		self.sub = rospy.Subscriber('/pos', Pos, self.callback)
-		
+	
+	##
+	# \brief Callback function for goal topic subscriber.
+    # \param msg: A Point message containing the goal coordinates.
+	#
+	# This function is the callback for the /goal_topic subscriber. It updates the target position variables with the values received in the message.
 	def goal_callback(self, msg):
 		# Upload the target position
 		self.des_x = msg.x
 		self.des_y = msg.y
 		
-		
+	##
+	# \brief Callback function for pos subscriber.
+    # \param data: A Pos message containing the current position and velocity information.
+	#
+	# This function is the callback for the /pos subscriber. It calculates the Euclidean distance between the current position and the desired position,
+	# as well as the average velocity, and prints this information if enough time has passed since the last print.
 	def callback(self, data):
 		# Get the current time in milliseconds
 		current_time = time.time() * 1000
